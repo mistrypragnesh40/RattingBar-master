2a638ee4f93d295b677ed9e1cd0ae62380c9195c
Mistry Pragnesh <mistrypragnesh40@gmail.com>
2020-09-13 12:01:25 +0530

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using App1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new RattingBarViewModal();
+        }
+    }
+}
---
+++ App1/App1/ViewModel/RattingBarViewModal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1.ViewModel
+{
+    public class RattingBarViewModal : INotifyPropertyChanged
+    {
+        public ICommand clickCommand { get; set; }
+        public RattingBarViewModal()
+        {
+            clickCommand = new Command(onClicked); // this will execute on the click of Click me button.
+        }
+
+        void onClicked(object obj)
+        {
+            RattingBar b = (RattingBar)obj;
+            App.Current.MainPage.DisplayAlert("Selected Value is", b.SelectedStarValue.ToString(), "OK");
+        }
+
+
+        private string _selectedStar;
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public string SelectedStar
+        {
+            get { return _selectedStar; }
+            set { _selectedStar = value; NotifyPropertyChanged(); }
+        }
+    }
+}
